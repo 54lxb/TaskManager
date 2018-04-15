36e1619bc096c942baec46bfdd4c587afa18853e
54lxb <1025502782@qq.com>
2018-04-15 16:52:52 +0800

    修改项目列表动画bug

--- src/app/anima/card.anima.ts
+++ src/app/anima/card.anima.ts
@@ -1,8 +1,14 @@
 import { trigger, state, transition, style, animate, keyframes } from "@angular/animations";
 
 export const cardAnima = trigger('card', [
-    state('out', style({transform: 'scale(1)', 'box-shadow' : 'none'})),
-    state('hover', style({transform: 'scale(1.1)', 'box-shadow' : '0.3em 0.3em 0.5em 0.6em #ccc'})),
-    transition('hover => out', animate('100ms ease-in')),
-    transition('out => hover', animate('100ms ease-out'))
+    state('out', style({
+        transform: 'scale(1)', 
+        'box-shadow' : 'none'
+    })),
+    state('hover', style({
+        transform: 'scale(1.1)', 
+        'box-shadow' : '0.3em 0.3em 0.3em 0.6em #ccc'
+    })),
+    transition('out => hover', animate('200ms ease-in')),
+    transition('hover => out', animate('200ms ease-out'))
 ])
--- src/app/anima/list.anima.ts
+++ src/app/anima/list.anima.ts
@@ -2,13 +2,15 @@ import { trigger, stagger, state, transition, style, animate, keyframes, group, 
 
 export const listAnima = trigger('listAnima', [
     transition('* => *', [
-        query(':enter', style({opacity: 0})),
+        query(':enter', style({opacity: 0}),{optional:true}),
         query(':enter', stagger(100, [
             animate('1s', style({opacity:1}))
-        ])),
-        query(':leave', style({opacity: 1})),
+        ]),{optional:true}),
+        query(':leave', style({opacity: 1}),{optional:true}),
         query(':leave', stagger(100, [
             animate('1s', style({opacity:0}))
-        ]))
+        ]),{optional:true})
     ])
 ])
+
+// stagger 同事添加多个元素时，控制元素按序出场，不然会所有元素听时出现；
