1b9bb837b53c0bfe2f92d29a15d173bfa8595a31
54lxb <1025502782@qq.com>
2018-04-15 10:54:13 +0800

    修改动画

--- src/app/anima/router.anima.ts
+++ src/app/anima/router.anima.ts
@@ -1,6 +1,6 @@
 import { trigger, state, transition, style, animate, keyframes } from "@angular/animations";
 
-export const itemAnima = trigger('itemAnima', [
+export const slideToRight = trigger('routerAnima', [
     state('in', style({'border-left-width' : '0.5em'})),
     state('out', style({'border-left-width' : '1.0em'})),
     transition('hover => out', animate('100ms ease-in')),
